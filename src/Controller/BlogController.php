10998e690e6df2fab0d6c12aa54df10198d620c6
deborah-maitrejean <deborah.maitrejean@gmail.com>
2018-09-07 20:03:20 +0200

    we can now save a post

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\News;
 use App\Repository\NewsRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
@@ -50,16 +52,30 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/blog/new", name="blog_create")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function create()
+    public function create(Request $request, ObjectManager $manager)
     {
-        $post = new News(); // nouvel article vide prêt à être rempli
+        $post = new News();
         $form = $this->createFormBuilder($post)
             ->add('title', TextType::class)
             ->add('content', TextareaType::class)
             ->add('image', TextType::class)
             ->getForm()
         ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) { // si le form est envoyé et valide
+            $post->setCreatedAt(new \DateTime());
+
+            $manager->persist($post); // faire persiter les données
+            $manager->flush(); // balancer la requête
+
+            return $this->redirectToRoute('blog_show', ['id' => $post->getId()]);
+        }
 
         return $this->render('blog/create.html.twig', [
             'postForm' => $form->createView()
